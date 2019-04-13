ac975d9847f4eba458216c04183a7e3113012aea
rohittavare <rohit.t797@yahoo.com>
2019-04-13 03:00:02 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# OpenCV-Install
+
+### A single script to install OpenCV on Ubuntu 18.04
+
+### Script take from [Learn OpenCV](https://www.learnopencv.com/install-opencv-4-on-ubuntu-18-04/)
+
+### Installation
+```bash
+# clone the repo
+git clone https://github.com/rohittavare/opencv-install.git
+# run install.sh
+sudo ./install.sh [VERSION]
+```
---
+++ install.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+if [ $# == 2 ]; then
+	cvVersion=$1
+else
+	cvVersion="master"
+fi
+echo "Installing OpenCV version $cvVersion"
+echo "---"
+echo "script compiled by Rohit Tavare, 2019 taken from Learn OpenCV"
+echo "Learn OpenCV has great resources for OpenCV. consider visiting at https://www.learnopencv.com/"
+rm -rf opencv/build
+rm -rf opencv_contrib/build
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y remove x264 libx264-dev
+sudo apt -y install build-essential checkinstall cmake pkg-config yasm
+sudo apt -y install git gfortran
+sudo apt -y install libjpeg8-dev libpng-dev
+
+sudo apt -y install software-properties-common
+sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+sudo apt -y update
+
+sudo apt -y install libjasper1
+sudo apt -y install libtiff-dev
+
+sudo apt -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt -y install libxine2-dev libv4l-dev
+cd /usr/include/linux
+sudo ln -s -f ../libv4l1-videodev.h videodev.h
+
+sudo apt -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt -y install libgtk2.0-dev libtbb-dev qt5-default
+sudo apt -y install libatlas-base-dev
+sudo apt -y install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt -y install libvorbis-dev libxvidcore-dev
+sudo apt -y install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt -y install libavresample-dev
+sudo apt -y install x264 v4l-utils
+
+sudo apt -y install libprotobuf-dev protobuf-compiler
+sudo apt -y install libgoogle-glog-dev libgflags-dev
+sudo apt -y install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo -H pip3 install -U pip numpy
+
+sudo pip3 install virtualenv virtualenvwrapper
+echo "# Virtual Environment Wrapper"  >> ~/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+source ~/.bashrc
+
+mkvirtualenv OpenCV-"$cvVersion"-py3 -p python3
+workon OpenCV-"$cvVersion"-py3
+pip install wheel numpy scipy matplotlib scikit-image scikit-learn ipython dlib
+deactivate
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout $cvVersion
+cd ..
+ 
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout $cvVersion
+cd ..
+
+cd opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D INSTALL_C_EXAMPLES=ON \
+      -D INSTALL_PYTHON_EXAMPLES=ON \
+      -D WITH_TBB=ON \
+      -D WITH_V4L=ON \
+      -D WITH_QT=ON \
+      -D WITH_OPENGL=ON \
+      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+      -D BUILD_EXAMPLES=ON ..
+
+CORES=`nproc`
+make -j $CORES
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
